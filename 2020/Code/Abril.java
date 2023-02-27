943b22c74d4982e315fcb3ed8d27b39d5c603daa
bgarci03 <125494283+bgarci03@users.noreply.github.com>
2023-02-27 11:23:53 -0700

    Add files via upload

---
+++ 2020/Code/Abril.java
@@ -0,0 +1,37 @@
+public class Abril
+{
+  public static void main(String[] args)
+  {
+    System.out.println("    UIL 2020");
+    System.out.println("  Java Program");
+    System.out.println("    Launch!");
+    System.out.println("================");
+    System.out.println("       ^");
+    System.out.println("       ^");
+    System.out.println("       |");
+    System.out.println("      | |");
+    System.out.println("     |   |");
+    System.out.println("    |_____|");
+    System.out.println("    |     |");
+    System.out.println("    |     |");
+    System.out.println("    |  U  |");
+    System.out.println("    |  I  |");
+    System.out.println("    |  L  |");
+    System.out.println("    |     |");
+    System.out.println("    |  T  |");
+    System.out.println("    |  X  |");
+    System.out.println("    |     |");
+    System.out.println("    |     |");
+    System.out.println("    [-----]");
+    System.out.println("     |###|");
+    System.out.println("      |#|");
+    System.out.println("       #");
+    System.out.println("      <#>");
+    System.out.println("     (###)");
+    System.out.println("    {~~~~~}");
+    System.out.println("   {~~}  {~~}");
+    System.out.println("  {~~}    {~~}");
+    System.out.println(" {~~~~}  {~~~~}");
+    System.out.println("{~~~~~}  {~~~~~}");
+  }
+}
