1ffcf33a31439bdf49ea3ca149309cf5e9966a8b
Brandon Garcia <125494283+bgarci03@users.noreply.github.com>
2023-03-09 20:21:57 -0700

    Add files via upload

--- 2020/Code/Abril.java
+++ 2020/Code/Abril.java
@@ -1,37 +1,43 @@
+import static java.lang.System.out;
+
 public class Abril
 {
   public static void main(String[] args)
   {
-    System.out.println("    UIL 2020");
-    System.out.println("  Java Program");
-    System.out.println("    Launch!");
-    System.out.println("================");
-    System.out.println("       ^");
-    System.out.println("       ^");
-    System.out.println("       |");
-    System.out.println("      | |");
-    System.out.println("     |   |");
-    System.out.println("    |_____|");
-    System.out.println("    |     |");
-    System.out.println("    |     |");
-    System.out.println("    |  U  |");
-    System.out.println("    |  I  |");
-    System.out.println("    |  L  |");
-    System.out.println("    |     |");
-    System.out.println("    |  T  |");
-    System.out.println("    |  X  |");
-    System.out.println("    |     |");
-    System.out.println("    |     |");
-    System.out.println("    [-----]");
-    System.out.println("     |###|");
-    System.out.println("      |#|");
-    System.out.println("       #");
-    System.out.println("      <#>");
-    System.out.println("     (###)");
-    System.out.println("    {~~~~~}");
-    System.out.println("   {~~}  {~~}");
-    System.out.println("  {~~}    {~~}");
-    System.out.println(" {~~~~}  {~~~~}");
-    System.out.println("{~~~~~}  {~~~~~}");
+    /*
+     * All of this code can be made in a single line,
+     * but is like this to provide visuality
+     */
+    out.println("    UIL 2020");
+    out.println("  Java Program");
+    out.println("    Launch!");
+    out.println("================");
+    out.println("       ^");
+    out.println("       ^");
+    out.println("       |");
+    out.println("      | |");
+    out.println("     |   |");
+    out.println("    |_____|");
+    out.println("    |     |");
+    out.println("    |     |");
+    out.println("    |  U  |");
+    out.println("    |  I  |");
+    out.println("    |  L  |");
+    out.println("    |     |");
+    out.println("    |  T  |");
+    out.println("    |  X  |");
+    out.println("    |     |");
+    out.println("    |     |");
+    out.println("    [-----]");
+    out.println("     |###|");
+    out.println("      |#|");
+    out.println("       #");
+    out.println("      <#>");
+    out.println("     (###)");
+    out.println("    {~~~~~}");
+    out.println("   {~~} {~~}");
+    out.println("  {~~}   {~~}");
+    out.println(" {~~~~} {~~~~}");
+    out.println("{~~~~~} {~~~~~}");
   }
 }
---
+++ 2020/Code/Emmanuel.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Emmanuel
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+
+    int cases = Integer.parseInt(sc.nextLine());
+
+    String[] output = new String[cases];
+
+    int index = 0;
+
+    for (int i = 0; i < cases; i++)
+    {
+      String input = sc.nextLine();
+      String[] array = input.substring(1, input.indexOf("}")).split(",");
+
+      int modeIndex = 0;
+      int timesAppeared = 0;
+
+      for (int element = 0; element < array.length; element++)
+      {
+        int count = 0;
+
+        for (int pos = 0; pos < array.length; pos++)
+          if (array[element].equals(array[pos]))
+            count++;
+        
+        if (count > timesAppeared)
+        {
+          timesAppeared = count;
+          modeIndex = element;
+        }
+      }
+
+      output[index] = array[modeIndex] + " appeared " + timesAppeared + " time(s)";
+      index++;
+    }
+
+    for (String result : output)
+      out.println(result);
+
+    sc.close();
+  } 
+}
